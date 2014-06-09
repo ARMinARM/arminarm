f9f0fa92998876bbadb9c5751db48dcfffc185fc
ARMinARM <arminarm@sociotope.com>
2014-06-09 22:23:08 +0200

    path_env

--- scripts/path_env.sh
+++ scripts/path_env.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-export PATH="$PATH:/opt/arminarm/bin"
-export PATH="$PATH:/opt/arminarm/tools"
+#export PATH=$PATH:/opt/arminarm/bin
+#export PATH=$PATH:/opt/arminarm/tools
+
+cd ~
+echo -n ".bashrc: "
+if [ -f .bashrc.bak ]; then
+  echo "Backup exists: not overwriting"
+  echo $PATH
+else
+  cp -a .bashrc .bashrc.bak
+  echo "PATH=\$PATH:/opt/arminarm/bin:/opt/arminarm/tools" >> .bashrc
+  echo $PATH
+fi
