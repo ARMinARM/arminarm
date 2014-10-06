4dac0f00139aca2c2f4f04e320ebf97e11b7ee84
ARMinARM <arminarm@sociotope.com>
2014-10-06 20:44:11 +0200

    Espruino to upstream github

--- src/update_espruino.sh
+++ src/update_espruino.sh
@@ -3,17 +3,14 @@
 if [ -d Espruino ]; then
 	echo "Directory exists: updating"
 	cd Espruino
-	git checkout arminarm
 	git pull
-#	ARMINARM=1 RELEASE=1 make
+	ARMINARM=1 RELEASE=1 make
 	cd ..
 	
 else
 	echo "First time git clone: installing"
-	git clone https://github.com/ARMinARM/Espruino/
+	git clone https://github.com/espruino/Espruino/
 	cd Espruino
-	git checkout arminarm
-#	ARMINARM=1 RELEASE=1 make
+	ARMINARM=1 RELEASE=1 make
 	cd ..
 fi
-
