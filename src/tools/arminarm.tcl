ed2decd848e7ebe94157de32a39e719c04dc3bdc
ARMinARM <arminarm@sociotope.com>
2014-10-30 00:16:18 +0100

    new openocd

--- src/tools/arminarm.tcl
+++ src/tools/arminarm.tcl
@@ -1,5 +1,5 @@
-source [find /usr/share/openocd/tcl/interface/sysfsgpio-raspberrypi.cfg]
+source [find /usr/share/openocd/scripts/interface/sysfsgpio-raspberrypi.cfg]
 set _CHIPNAME stm32f1x
 
-source [find /usr/share/openocd/tcl/target/stm32f1x.cfg]
+source [find /usr/share/openocd/scripts/target/stm32f1x.cfg]
 $_TARGETNAME configure -event gdb-attach { reset init }
--- src/update_openocd.sh
+++ src/update_openocd.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-#git clone --recursive git://git.code.sf.net/p/openocd/code openocd
-#cd openocd
-
-if [ -d openocd-arminarm ]; then
+if [ -d openocd ]; then
 	echo "Directory exists: updating"
-	cd openocd-arminarm
+	cd openocd
 	git pull
 	# assume we're already bootstrapped and configured
 else
 	echo "First time git clone: installing"
-	git clone https://github.com/ARMinARM/openocd-arminarm
-	cd openocd-arminarm
+#	git clone git://git.code.sf.net/p/openocd/code openocd
+	git clone https://github.com/ARMinARM/openocd
+	cd openocd
 	./bootstrap
-	./configure --enable-sysfsgpio --enable-maintainer-mode --disable-werror --enable-ep93xx --enable-at91rm9200 --enable-usbprog --enable-presto_libftdi --enable-jlink --enable-vsllink --enable-rlink --enable-arm-jtag-ew --enable-dummy --enable-buspirate --enable-ulink --enable-presto_libftdi --enable-usb_blaster_libftdi --prefix=/usr
+	./configure --enable-sysfsgpio --enable-bcm2835gpio --enable-buspirate --prefix=/usr
 fi
 
 make
 sudo make install
 
-sudo cp -r tcl/ /usr/share/openocd
-
+cd ..
 
 # usage:
-# sudo openocd -s /usr/share/openocd/tcl -f /opt/arminarm/tools/arminarm.tcl
+# sudo openocd -s /usr/share/openocd/scripts -f /opt/arminarm/tools/arminarm.tcl
 
 # arm-none-eabi-gdb
 # target extended-remote localhost:3333
-# file ... etc
+# [do your thing]
 
