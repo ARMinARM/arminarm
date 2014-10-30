b17a9ce2d305064cdc6da9bd50434a07a93457c7
ARMinARM <arminarm@sociotope.com>
2014-10-30 02:35:29 +0100

    openocd patches

--- src/update_openocd.sh
+++ src/update_openocd.sh
@@ -9,6 +9,13 @@ else
 	echo "First time git clone: installing"
 	git clone git://git.code.sf.net/p/openocd/code openocd
 	cd openocd
+	
+	wget https://raw.githubusercontent.com/ARMinARM/openocd-arminarm/master/tcl/interface/sysfsgpio-raspberrypi.cfg
+	mv sysfsgpio-raspberrypi.cfg tcl/interface
+	
+	wget https://raw.githubusercontent.com/ARMinARM/openocd-arminarm/master/tcl/interface/raspberrypi-native.cfg
+	mv raspberrypi-native.cfg tcl/interface
+	
 	./bootstrap
 	./configure --enable-sysfsgpio --enable-bcm2835gpio --enable-buspirate --prefix=/usr
 fi
