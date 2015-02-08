92e0278030f7ff1b26b715b8ab6ae82b5db8df12
ARMinARM <rik@onandoffables.com>
2015-02-08 20:06:13 +0100

    2708/2709 detect

--- bin/install_toolchain.sh
+++ bin/install_toolchain.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-ARMINARM=arm-toolchain-bin-2014-06-20-raspbian.tar.gz
-#ARMINARM=arm-toolchain-bin-2015-02-04-raspbian.tar.gz
-URL=https://github.com/ARMinARM/arminarm-bin/raw/master/$ARMINARM
+PROCESSOR=`cat /proc/cpuinfo | grep Hardware | awk '{ print $3 }'`
+
+if [ "$PROCESSOR" == "BCM2708" ]; then
+	echo "$PROCESSOR found: Pi 1"
+	ARMINARM=arm-toolchain-bin-2014-06-20-raspbian.tar.gz
+elif [ "$PROCESSOR" == "BCM2709" ]; then
+	echo "$PROCESSOR found: Pi 2"
+	ARMINARM=arm-toolchain-bin-2015-02-04-raspbian.tar.gz
+else
+	echo "unknown processor"
+	ARMINARM=arm-toolchain-bin-2014-06-20-raspbian.tar.gz
+	exit
+fi
+
+URL=https://raw.githubusercontent.com/ARMinARM/arminarm-bin/master/$ARMINARM
 
 if [ ! -f $ARMINARM ]; then
 	echo "toolchain tar.gz not found. Getting it from github."
