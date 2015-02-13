2ea387da7a2887a0f18dbd37a63467800152646e
ARMinARM <rik@onandoffables.com>
2015-02-13 14:03:26 +0100

    espruino_1v73

--- bin/install_espruino_bin.sh
+++ bin/install_espruino_bin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ESPRUINO=espruino_1v72_ARMinARM.bin
+ESPRUINO=espruino_1v73_ARMinARM.bin
 URL=https://github.com/ARMinARM/arminarm-bin/raw/master/$ESPRUINO
 
 if [ ! -f $ESPRUINO ]; then
