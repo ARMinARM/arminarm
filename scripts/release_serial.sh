adcd719c61afc7b321ab8ee7a400b663adaf9334
ARMinARM <rik@onandoffables.com>
2015-12-03 14:38:30 +0000

    do_serial from raspi-config

--- scripts/release_serial.sh
+++ scripts/release_serial.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-RPISERIALCONSOLE=rpi-serial-console
-URL=https://raw.githubusercontent.com/ARMinARM/$RPISERIALCONSOLE/master/$RPISERIALCONSOLE
+#from: https://github.com/asb/raspi-config/blob/master/raspi-config
 
+do_serial() {
+	CURRENT_STATUS="yes" # assume ttyAMA0 output enabled
+	if ! grep -q "^T.*:.*:respawn:.*ttyAMA0" /etc/inittab; then
+		CURRENT_STATUS="no"
+	fi
 
-if [ ! -f $RPISERIALCONSOLE ]; then
-	echo "rpi-serial-console not found. Getting it from github."
-	wget $URL
-	chmod +x $RPISERIALCONSOLE
-	#sudo cp $RPISERIALCONSOLE /usr/bin/$RPISERIALCONSOLE
-fi
+	#whiptail --yesno "Would you like a login shell to be accessible over serial?" 20 60 2
+	#RET=$?
+	RET=$1
+	if [ $RET -eq 1 ]; then
+		sed -i /etc/inittab -e "s|^.*:.*:respawn:.*ttyAMA0|#&|"
+		sed -i /boot/cmdline.txt -e "s/console=ttyAMA0,[0-9]\+ //"
+		echo "Serial is now disabled. Please reboot!"
+	elif [ $RET -eq 0 ]; then
+		sed -i /etc/inittab -e "s|^#\(.*:.*:respawn:.*ttyAMA0\)|\1|"
+		if ! grep -q "^T.*:.*:respawn:.*ttyAMA0" /etc/inittab; then
+			printf "T0:23:respawn:/sbin/getty -L ttyAMA0 115200 vt100\n" >> /etc/inittab
+		fi
+		if ! grep -q "console=ttyAMA0" /boot/cmdline.txt; then
+			sed -i /boot/cmdline.txt -e "s/root=/console=ttyAMA0,115200 root=/"
+		fi
+		echo "Serial is now enabled. Please reboot!"
+	else
+		return $RET
+	fi
+}
 
-sudo ./$RPISERIALCONSOLE $1
+do_serial $1
